7592be64ed25a21b23fb3e899d23b93a051430ed
kapline <true.kapline@gmail.com>
2025-04-17 12:19:28 +0800

    Оптимизация архитектуры и создание компонента аутентификации

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../@libs/components/login/login.component';
 import { RegistrationComponent } from '../@libs/components/registration/registration.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProfileComponent } from './profile/profile.component';
-import { CoursesComponent } from './courses/courses.component';
-import { StudyPlanComponent } from './study-plan/study-plan.component';
-import { DefaultComponent } from './default/default.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { StudyPlanComponent } from './components/study-plan/study-plan.component';
+import { DefaultComponent } from './components/default/default.component';
 import { EmptyLayoutComponent } from '../@libs/layouts/empty-layout/empty-layout.component';
 import { MainLayoutComponent } from '../@libs/layouts/main-layout/main-layout.component';
+import { AuthComponent } from './components/auth/auth.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,7 @@ export const routes: Routes = [
     component: EmptyLayoutComponent,
     children: [
       { path: '', component: DefaultComponent },
+      { path: 'auth', component: AuthComponent },
       { path: 'login', component: LoginComponent },
       { path: 'registration', component: RegistrationComponent },
     ],
