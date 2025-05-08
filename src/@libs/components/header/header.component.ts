6495ab9b3006f127ec61792386b457d9f41e97fa
Kapline <true.kapline@gmail.com>
2025-05-09 01:16:41 +0800

    Исправлены мелкие баги со сменой темы

--- src/@libs/components/header/header.component.ts
+++ src/@libs/components/header/header.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'krate-header',
   imports: [
-    NgOptimizedImage,
     RouterLink,
     RouterLinkActive,
   ],
